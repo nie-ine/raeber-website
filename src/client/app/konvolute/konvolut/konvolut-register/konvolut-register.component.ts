db7d48edd960afef155656745aa8d2035632ed73
Reto Baumgartner <rfbaumgartner@users.noreply.github.com>
2017-06-19 17:06:48 +0200

    add convolute register, convolute grid including mock data

---
+++ src/client/app/konvolute/konvolut/konvolut-register/konvolut-register.component.ts
@@ -0,0 +1,18 @@
+/**
+ * Created by retobaumgartner on 19.06.17.
+ */
+import {Component, Input} from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'rae-konvolut-register',
+  templateUrl: 'konvolut-register.component.html',
+  styleUrls: ['konvolut-register.component.css']
+})
+export class KonvolutRegisterComponent {
+  selectedRow: number;
+
+  constructor() {
+
+  }
+}
--- src/client/app/konvolute/konvolut/konvolut.module.ts
+++ src/client/app/konvolute/konvolut/konvolut.module.ts
@@ -6,13 +6,17 @@ import { NgModule } from '@angular/core';
 
 import { KonvolutRoutingModule } from './konvolut-routing.module';
 import { KonvolutComponent } from './konvolut.component';
-import {KonvolutSteckbriefComponent} from "./konvolut-steckbrief/konvolut-steckbrief.component";
-import {MdCardModule} from "@angular/material";
+import {KonvolutSteckbriefComponent} from './konvolut-steckbrief/konvolut-steckbrief.component';
+import {MdButtonToggleModule, MdCardModule, MdGridListModule, MdListModule} from '@angular/material';
+import {KonvolutGridComponent} from './konvolut-grid/konvolut-grid.component';
+import {KonvolutRegisterComponent} from './konvolut-register/konvolut-register.component';
 
 @NgModule({
-  imports: [ KonvolutRoutingModule, MdCardModule ],
+  imports: [ KonvolutRoutingModule, MdCardModule, MdGridListModule, MdButtonToggleModule, MdListModule ],
   declarations: [ KonvolutComponent,
-  KonvolutSteckbriefComponent],
+  KonvolutSteckbriefComponent,
+  KonvolutGridComponent,
+  KonvolutRegisterComponent],
   exports: [ KonvolutComponent ],
   providers: []
 })

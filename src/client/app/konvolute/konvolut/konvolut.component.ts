74e485425f5d94a91046788c13f1d82bc007f8ac
Sebastian Schüpbach <sebastian.schuepbach@unibas.ch>
2017-07-18 16:15:50 +0200

    Adds utility class for creating Knora API requests and replaces direct search requests in components with instances of the class.

--- src/client/app/konvolute/konvolut/konvolut.component.ts
+++ src/client/app/konvolute/konvolut/konvolut.component.ts
@@ -1,18 +1,21 @@
 /**
  * Created by retobaumgartner on 06.06.17.
  */
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
+import { DynamicPaging } from '../../shared/utilities/paging';
+import { ExtendedSearch, FulltextSearch, KnoraProperty } from '../../shared/utilities/knora-api-params';
 
 @Component({
   moduleId: module.id,
   selector: 'rae-konvolut',
-  templateUrl: 'konvolut.component.html'
+  templateUrl: 'konvolut.component.html',
+  providers: [ DynamicPaging ]
 })
 export class KonvolutComponent implements OnInit {
 
@@ -25,24 +28,36 @@ export class KonvolutComponent implements OnInit {
   konvolut_type: string;
   private sub: any;
 
-  constructor(private http: Http, private route: ActivatedRoute, private router: Router) {
+  private _req = new ExtendedSearch();
+
+  constructor(private http: Http, private route: ActivatedRoute, private router: Router, private dp: DynamicPaging, lc: NgZone) {
+    window.onscroll = () => {
+      let status = 'not reached';
+      let windowHeight = 'innerHeight' in window ? window.innerHeight
+        : document.documentElement.offsetHeight;
+      let body = document.body, html = document.documentElement;
+      let docHeight = Math.max(body.scrollHeight,
+        body.offsetHeight, html.clientHeight,
+        html.scrollHeight, html.offsetHeight);
+      let windowBottom = windowHeight + window.pageYOffset;
+      if (windowBottom >= docHeight) {
+        this.loadMore();
+      }
+    };
   }
 
   ngOnInit() {
-    this.konvolut_type = this.route.snapshot.url[ 0 ].path;
-
-    this.route.params
-      .switchMap((params: Params) =>
-        this.http.get('http://localhost:3333/v1/search/?searchtype=extended&' +
-          'filter_by_restype=http%3A%2F%2Fwww.knora.org%2Fontology%2Ftext%23Convolute&' +
-          'property_id=http%3A%2F%2Fwww.knora.org%2Fontology%2Ftext%23hasTitle&compop=!EQ&searchval=%20&' +
-          'property_id=http%3A%2F%2Fwww.knora.org%2Fontology%2Ftext%23hasDescription&compop=!EQ&searchval=%20&show_nrows=500'))
-      .map(response => response.json().subjects)
-      .subscribe((res: Array<any>) => this.poems = res);
+    this._req.filterByRestype = 'http://www.knora.org/ontology/text#Convolute';
+    this._req.property = new KnoraProperty('http://www.knora.org/ontology/text#hasTitle', '!EQ', ' ');
+    this._req.property = new KnoraProperty('http://www.knora.org/ontology/text#hasDescription', '!EQ', ' ');
+    this.dp.size = 10;
+    this.dp.loadText(this._req).subscribe(
+      konstText => this.poems = konstText
+    );
 
     this.konvolut_type = this.route.snapshot.url[ 0 ].path;
     this.sub = this.route.params.subscribe(params => {
-      this.konvolut_id = params[ 'konvolut' ];
+      this.konvolut_id = params[ 'id' ];
     });
 
     console.log('search/schlaf?searchtype=fulltext');
@@ -50,17 +65,25 @@ export class KonvolutComponent implements OnInit {
 
   // for testings
   searchForDoctor(fulltextQuery: string) {
-    this.http.get('http://localhost:3333/v1/search/' + fulltextQuery + '?searchtype=fulltext')
+    let searchParams = new FulltextSearch;
+    searchParams.searchstring = fulltextQuery;
+    this.http.get(searchParams.toString())
       .map(response => response.json().subjects)
       .subscribe(res => this.poems = res);
-    console.log('/search/' + fulltextQuery + '?searchtype=fulltext');
+    console.log(searchParams.toString());
   }
 
   // for testings
   freeSearch() {
-    this.http.get('http://localhost:3333/v1/search/' + this.searchQuery)
+    this.http.get('http://knora.nie-ine.ch/v1/search/' + this.searchQuery)
       .map(response => response.json().subjects)
       .subscribe(res => this.poems = res);
     console.log('/search/' + this.searchQuery);
+  }
+
+  loadMore() {
+    this.dp.loadText(this._req).subscribe(
+      konstText => this.poems = this.poems.concat(konstText)
+    );
   }
 }

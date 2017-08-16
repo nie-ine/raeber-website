18df5987c6cb59133bb4ae6b8470b4f4ac0ee565
rfbaumgartner <rfbaumgartner@users.noreply.github.com>
2017-08-16 14:31:27 +0200

    cleanup and comment searchstring parsing

--- src/client/app/suche/utilities/parse-searchstring.service.ts
+++ src/client/app/suche/utilities/parse-searchstring.service.ts
@@ -6,45 +6,76 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ParseSearchstringService {
 
-  parseSearchString(searchString: string) {
-    let quoteSplit = searchString.split('"');
-    let queryWords: Array<any> = [];
+  // expects a searchquery like '"palm trees" apples OR intext: pears'
+  // returns an array of an array containing a map per word like
+  // [ [{searchString: "palm trees", where: "anywhere"}, {searchString: "apples", where: "anywhere"}],
+  //   [{searchString: "pears", where: "text"}] ]
+
+  parseSearchString(unparsedSearch: string) {
+
+    let quoteSplit = unparsedSearch.split('"'); // quotation marks have the highest precedency. Split on them first.
+    let queryWords: Array<any> = []; // list for the words of the unparsed search
+
     for (let i = 0; i < quoteSplit.length; i++) {
+
       if (i % 2 === 0 && quoteSplit[i] !== '') {
+        // words in even places are considered as outside of quotation marks
+
         let restWordSplit = quoteSplit[i].trim().split(' ');
         for (let j = 0; j < restWordSplit.length; j++) {
+
           if (restWordSplit[j] === 'intitle:' || restWordSplit[j] === 'intext:' || restWordSplit[j] === 'OR') {
+            // functional words are marked with where:open
             queryWords.push({'searchString': restWordSplit[j], 'where': 'open'});
           } else {
+            // non-functional words are defaulted to be looked for everywhere
             queryWords.push({'searchString': restWordSplit[j], 'where': 'anywhere'});
           }
         }
       } else if (quoteSplit[i] !== '') {
+        // words in odd places are considered to be in quotation marks
         queryWords.push({'searchString': quoteSplit[i], 'where': 'anywhere'});
       }
     }
 
+    // cleanup functional words
     for (let i = 0; i < queryWords.length; i++) {
       if (queryWords[i].searchString === 'intitle:' && queryWords[i].where === 'open' && queryWords[i+1]) {
+        // if there is a word after, intitle is set to that word's property and marked to be deleted
         queryWords[ i ].where = 'delete';
         queryWords[ i + 1 ].where = 'title';
+      } else if (queryWords[i].searchString === 'intitle:' && queryWords[i].where === 'open' && i === queryWords.length) {
+        // if intitle: is the last word it is treated like a normal word
+        queryWords[ i ].where = 'anywhere';
       } else if (queryWords[i].searchString === 'intext:' && queryWords[i].where === 'open' && queryWords[i+1]) {
+        // if there is a word after, intext is set to that word's property and marked to be deleted
         queryWords[ i ].where = 'delete';
         queryWords[ i + 1 ].where = 'text';
+      } else if (queryWords[i].searchString === 'intext:' && queryWords[i].where === 'open' && i === queryWords.length) {
+        // if intext: is the last word it is treated like a normal word
+        queryWords[ i ].where = 'anywhere';
+      } else if (queryWords[i].searchString === 'OR' && queryWords[i].where === 'open' && i === 0) {
+        // if OR is the first word it is treated like a normal word
+        queryWords[ i ].where = 'anywhere';
+      } else if (queryWords[i].searchString === 'OR' && queryWords[i].where === 'open' && i === queryWords.length) {
+        // if OR is the last word it is treated like a normal word
+        queryWords[ i ].where = 'anywhere';
       }
     }
 
-    let queries: Array<Array<any>> = [];
-    let queryEntry: Array<any> = [];
+    let queries: Array<Array<any>> = []; // queries divided by OR
+    let queryEntry: Array<any> = []; // query words divided by AND
 
     for (let i = 0; i < queryWords.length; i++) {
-      if (queryWords[i].searchString === 'OR' && queryWords[i].where === 'open' && i !== 0) {
+      if (queryWords[i].searchString === 'OR' && queryWords[i].where === 'open' && i !== 0 && i !== queryWords.length) {
+        // on OR (if not in the first or last element) a new query array is started
+
         queries.push(queryEntry);
         queryEntry = [];
-      } else if (queryWords[i].searchString === 'OR' && queryWords[i].where === 'open' && i === 0) {
-        queryEntry.push({'searchString': queryWords[i], 'where': 'anywhere'});
       } else if (queryWords[i].where !== 'delete') {
-        queryEntry.push({'searchString': queryWords[i], 'where': queryWords[i].where});
+        // all normal words of the query are collected in the AND list
+
+        queryEntry.push({'searchString': queryWords[i].searchString, 'where': queryWords[i].where});
       }
     }
 

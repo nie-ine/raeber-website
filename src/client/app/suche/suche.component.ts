ee09498bff5f4f892f06130f0580bf3c52146d8c
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2017-08-17 16:38:44 +0200

    weitere Verbesserungen

--- src/client/app/suche/suche.component.ts
+++ src/client/app/suche/suche.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { globalSearchVariableService } from './globalSearchVariablesService';
 import {isUndefined} from "util";
+import {forEach} from "@angular/router/src/utils/collection";
 
 
 
@@ -51,16 +52,13 @@ export class SucheComponent implements OnInit {
   isAlreadyInArray = 0;
   //setOfAllQueries: Array<any>; //{numberOfSearchBox: '', numberOfProperty: '', propertyIRI: '', logicalOperator: '', searchVal: ''}
   helperMap = new Map();
-  setOfAllQueries = {
-    searchBox: '',
-    propertyNumber: '',
-      property: '',
-      compOp: '',
-      value: ''
-  };
+  mapOfAllQueries = new Map();
   count = 0;
   numberOfPropertiesInSearchBox = '';
   str: string;
+  value: string;
+  keys: Array<any>;
+  finalQueryArray= [''];
 
   constructor(private http: Http) {
   }
@@ -144,77 +142,33 @@ export class SucheComponent implements OnInit {
   }
 
   updateQuerySet(propertyTriple: Array<any>) {
+    this.k = 0;
     // Case: setOfAllQueries it totally empty:
     console.log('PropertyTriple: ' + propertyTriple);
-    this.helperMap.set(
+    this.mapOfAllQueries.set(
       propertyTriple[0].toString() + propertyTriple[1].toString(), [
-          propertyTriple[2], [
-            propertyTriple[3], propertyTriple[4]
-            ]
+        propertyTriple[2], [
+          propertyTriple[3], propertyTriple[4]
+        ]
       ]
     );
-    this.str = JSON.stringify(this.helperMap, null, 4);
+    this.str = JSON.stringify(this.mapOfAllQueries, null, 4);
     console.log(this.str);
-
-    /*this.helperMap.searchBox = propertyTriple[0];
-    this.helperMap.propertyNumber = propertyTriple[1];
-    this.helperMap.property = propertyTriple[2];
-    this.helperMap.compOp = propertyTriple[3];
-    this.helperMap.value = propertyTriple[4];
-    this.setOfAllQueries.push(this.helperMap);
-    this.str = JSON.stringify(this.setOfAllQueries, null, 4);
-    console.log(this.str);*/
-    /* if ( this.setOfAllQueries === undefined) {
-      this.setOfAllQueries[0].propertyTriple[0] = propertyTriple[0];
-      this.setOfAllQueries[0].numberOfPropertiesInSearchBox = '1';
-      this.setOfAllQueries[0].
-      this.setOfAllQueries[0].propertyIRI = propertyTriple[2]
-      console.log('Set first Entry');
-      this.setOfAllQueries=propertyTriple;
-    } else {
-      this.isAlreadyInArray = 0;
-      for (this.i = 0; this.i < this.setOfAllQueries.length; this.i += propertyTriple.length) {
-            // Check if entry exists for respective searchox
-            if ( this.setOfAllQueries[this.i] === propertyTriple[0]) {
-              console.log('Update Query');
-              this.isAlreadyInArray = 1;
-              //Check if it's a new property or if the old one is updated
-              if( this.setOfAllQueries[ this.i+ 1 ]  === propertyTriple[1]) {
-                console.log('Update Property');
-              this.k = 0;
-              for(this.j = this.i; this.j < this.i + propertyTriple.length; this.j++) {
-                this.setOfAllQueries[this.j] = propertyTriple[this.k];
-                this.k++;
-              }
-              } else {
-                console.log('Add Property');
-              }
-              //this.setOfAllQueries.concat(propertyTriple);
-              console.log('Searchbox is already in setOfQueires');
-            }
-          }
-      if (this.isAlreadyInArray === 0) {
-        console.log('Add new query');
-        console.log('setOfAllQueries before adding: ' + this.setOfAllQueries);
-        for(this.i = 0; this.i < propertyTriple.length; this.i++) {
-          this.setOfAllQueries.push(propertyTriple[this.i]);
+    //Final list of Queries:
+    this.keys = Array.from(this.mapOfAllQueries.keys());
+    console.log(this.keys);
+    this.mapOfAllQueries.forEach(
+      value => {
+        this.finalQueryArray[this.k] = this.keys[this.k];
+        console.log('Key: ' + this.finalQueryArray[this.k]);
+        this.k++;
+        console.log(value[0]);
+        for(this.i = 0; this.i < value[1].length; this.i++) {
+          console.log(value[1][this.i]);
         }
-        console.log('setOfAllQueries after adding: ' + this.setOfAllQueries);
       }
-    }
-    console.log('setOfAllQueries: ' + this.setOfAllQueries + 'Length: ' + this.setOfAllQueries.length);
-    console.log('----------');
-    for (this.i = 0; this.i < this.setOfAllQueries.length; this.i += propertyTriple.length) {
-      console.log(
-          'SearchBox Number: ' + this.setOfAllQueries[ this.i ]      + '\n'
-        + 'Property Number: '  + this.setOfAllQueries[ this.i + 1 ]  + '\n'
-        + 'Property IRI: '     + this.setOfAllQueries[ this.i + 2 ]  + '\n'
-        + 'Compare Operator: ' + this.setOfAllQueries[ this.i + 3 ]  + '\n'
-        + 'Search Value: '     + this.setOfAllQueries[ this.i + 4 ]  + '\n'
-        + '\n\n' );
-    }
-    console.log('----------');*/
-  }
+    );
 
+  }
 
 }

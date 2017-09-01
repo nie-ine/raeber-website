1f4a4beccf51449ae4d1d08aab389dad664be3f7
Sebastian <sebastian.schuepbach@unibas.ch>
2017-09-01 14:56:27 +0200

    Adds documentation and new buttons for disabling checkbox elements.

--- src/client/app/suche/suchmaske/suchmaske.component.ts
+++ src/client/app/suche/suchmaske/suchmaske.component.ts
@@ -21,29 +21,55 @@ import 'rxjs/add/operator/switchMap';
 })
 export class SuchmaskeComponent implements OnInit {
 
+  // Emits changes of search parameters
   @Output() public suchEvents: EventEmitter<AbstractControl> = new EventEmitter<AbstractControl>();
+  // Form model which contains all search parameters
   suchmenuForm: FormGroup;
-  showNotizbuecher = false;
-  buttonShow = this.showNotizbuecher ? 'fa-caret-down' : 'fa-caret-right';
-  showManuskripte = false;
-  showTyposkripte = false;
-  showSelbPub = false;
-  showUnselbPub = false;
-  showMaterialien = false;
-  showTextarten = true;
-  filterChangeLog: boolean[] = [];
+
+  /*
+  Options for extension of fulltext search
+   */
   suchraumOptions = [
     { value: 'volltext', viewValue: 'in Text & Titel' },
     { value: 'text', viewValue: 'in Text' },
     { value: 'titel', viewValue: 'in Titel' }
   ];
 
-  constructor(private fb: FormBuilder /*, private route: ActivatedRoute */) {
+  /*
+  Tracks if convolute category elements are hidden
+   */
+  catHidden = {
+    notizbuecher: false,
+    manuskripte: false,
+    typoskripte: false,
+    drucke: false,
+    zeitschriften: false,
+    materialien: false
+  };
+
+  /*
+  Tracks if convolute category elements are disabled
+   */
+  catDisabled = {
+    notizbuecher: false,
+    manuskripte: false,
+    typoskripte: false,
+    drucke: false,
+    zeitschriften: false,
+    materialien: false
+  };
+
+  constructor(private fb: FormBuilder) {
     this.createForm();
     this.onSearchParamsChange();
-    // this.logNotizbuchCbChange();
   }
 
+  /**
+   * Checks if all child elements have the same value
+   * @param {string} formGroupPath Path to respective `FormGroup`
+   * @param {string} parentFormControlName Name of parent `FormControl`
+   * @returns {boolean} True if all have the same value
+   */
   childElemsHaveSameValues(formGroupPath: string, parentFormControlName: string) {
     const children = (this.suchmenuForm.get(formGroupPath) as FormGroup).controls;
     let b: boolean;
@@ -61,6 +87,10 @@ export class SuchmaskeComponent implements OnInit {
     return true;
   }
 
+  /**
+   * Toggles disabled/enabled state of element
+   * @param {string} formControlPath Path to `FormControl`
+   */
   toggleDisabled(formControlPath: string) {
     if ((this.suchmenuForm.get(formControlPath) as FormControl).disabled) {
       this.suchmenuForm.get(formControlPath).enable();
@@ -69,6 +99,11 @@ export class SuchmaskeComponent implements OnInit {
     }
   }
 
+  /**
+   * Toggles disabled/enabled state of child elements
+   * @param {string} formGroupPath Path to respective `FormGroup`
+   * @param {string} parentFormControlName Path to respective parent `FormControl`
+   */
   toggleGroupDisabled(formGroupPath: string, parentFormControlName: string) {
     const children = (this.suchmenuForm.get(formGroupPath) as FormGroup).controls;
     if ((this.suchmenuForm.get(parentFormControlName) as FormControl).disabled) {
@@ -102,14 +137,22 @@ export class SuchmaskeComponent implements OnInit {
     return true;
   }
 
-  setValueForChildElems(path: string, allElemPath: string) {
-    const children = (this.suchmenuForm.get(path) as FormGroup).controls;
-    const allElemValue = (this.suchmenuForm.get(allElemPath) as FormControl).value;
+  /**
+   * Aligns the values of the child elements with the value of the parent element
+   * @param {string} formGroupPath Path to `FormGroup`
+   * @param {string} parentFormControlPath Path to parent `FormControl`
+   */
+  setValueForChildElems(formGroupPath: string, parentFormControlPath: string) {
+    const children = (this.suchmenuForm.get(formGroupPath) as FormGroup).controls;
+    const allElemValue = (this.suchmenuForm.get(parentFormControlPath) as FormControl).value;
     for (const c in children) {
       children[c].setValue(allElemValue, { emitModeltoViewChange: true });
     }
   }
 
+  /**
+   * Emits updated form model to caller
+   */
   onSearchParamsChange() {
     const suchmenuForm = (this.suchmenuForm as FormGroup).controls;
     for (const s in suchmenuForm) {
@@ -118,6 +161,9 @@ export class SuchmaskeComponent implements OnInit {
     }
   }
 
+  /**
+   * Creates form model
+   */
   createForm() {
     this.suchmenuForm = this.fb.group({
       suchwortForm: this.fb.group(new Suchwort()),

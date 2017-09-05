e3ebd685bb6d7f2f6a7765e9b7f96c3bdba4874e
rfbaumgartner <rfbaumgartner@users.noreply.github.com>
2017-09-05 11:34:27 +0200

    set to initial markup when a new poem is loaded

--- src/client/app/fassung/fassung-diplomatisch/fassung-diplomatisch.component.ts
+++ src/client/app/fassung/fassung-diplomatisch/fassung-diplomatisch.component.ts
@@ -1,16 +1,20 @@
 /**
  * Created by Reto Baumgartner (rfbaumgartner) on 24.07.17.
  */
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
   moduleId: module.id,
   selector: 'rae-fassung-diplomatisch',
   templateUrl: 'fassung-diplomatisch.component.html'
 })
-export class FassungDiplomatischComponent {
+export class FassungDiplomatischComponent implements OnChanges {
 
   @Input() pages: any;
   gewaehlteSchicht: string = 'schicht0';
+
+  ngOnChanges() {
+    this.gewaehlteSchicht = 'schicht0';
+  }
 
 }

0f638e649ae6108701b57db0a5f711a8bf2499d4
Sebastian <sebastian.schuepbach@unibas.ch>
2017-09-12 11:42:40 +0200

    Add a first draft of a basic search in header

---
+++ src/client/app/suche/basic-search/basic-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'rae-basic-search',
+  templateUrl: './basic-search.component.html'
+})
+export class BasicSearchComponent {
+
+}

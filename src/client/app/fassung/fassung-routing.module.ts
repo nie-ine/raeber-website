076796396253a9605ee05ab01657dc21002f1ffa
Sebastian <sebastian.schuepbach@unibas.ch>
2017-09-13 10:29:42 +0200

    Remove route to special collection `typoskripte-sammlungen`
    The collection `typoskripte-sammlungen` serves only as a ordering element in the navigation

--- src/client/app/fassung/fassung-routing.module.ts
+++ src/client/app/fassung/fassung-routing.module.ts
@@ -12,7 +12,6 @@ import { FassungComponent } from './fassung.component';
       { path: 'drucke/:konvolut/:fassung', component: FassungComponent },
       { path: 'manuskripte/:konvolut/:fassung', component: FassungComponent },
       { path: 'notizbuecher/:konvolut/:fassung', component: FassungComponent },
-      { path: 'typoskripte/typoskripte-sammlungen/:konvolut/:fassung', component: FassungComponent },
       { path: 'typoskripte/:konvolut/:fassung', component: FassungComponent },
       { path: 'material/:konvolut/:fassung', component: FassungComponent }
     ])

0703f3097e37f6c80a978e1991de0f38733783a1
Sebastian <sebastian.schuepbach@unibas.ch>
2017-09-21 18:08:07 +0200

    Get current route with the `url` method in `Router` class

--- src/client/app/shared/textgrid/textgrid.component.ts
+++ src/client/app/shared/textgrid/textgrid.component.ts
@@ -2,8 +2,7 @@
  * Created by retobaumgartner on 21.06.17.
  */
 
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import { ActivatedRoute, ParamMap, UrlSegment } from '@angular/router';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import 'rxjs/add/operator/switchMap';
 
 export class Konvolut {
@@ -17,7 +16,7 @@ export class Konvolut {
   templateUrl: 'textgrid.component.html',
   styleUrls: ['textgrid.component.css']
 })
-export class TextgridComponent implements OnChanges, OnInit {
+export class TextgridComponent implements OnChanges {
 
   @Input() contentType: string = 'suche'; // synopse OR konvolut OR suche
   @Input() viewMode: string = 'grid';
@@ -28,9 +27,6 @@ export class TextgridComponent implements OnChanges, OnInit {
   gridTextHeight: number = 10;
 
   convoluteRoute: string = '';
-
-  constructor(private route: ActivatedRoute) {
-  }
 
   ngOnChanges(changes: SimpleChanges) {
     for (let propName in changes) {
@@ -46,17 +42,6 @@ export class TextgridComponent implements OnChanges, OnInit {
      this.changeLog.push(`${propName}: currentValue = ${cur}, previousValue = ${prev}`);
      }*/
     // changes.prop contains the old and the new value...
-  }
-
-  ngOnInit(): void {
-    this.route.url
-      .switchMap((elems: UrlSegment[]) =>
-        (elems[0].path)).subscribe(convTypeFrag => this.convoluteRoute = this.convoluteRoute + convTypeFrag);
-    this.convoluteRoute = this.convoluteRoute + '/';
-    this.route.paramMap
-      .switchMap((params: ParamMap) =>
-        (params.getAll('konvolut'))).subscribe(konvolut => this.convoluteRoute = this.convoluteRoute + konvolut);
-    this.convoluteRoute = this.convoluteRoute + '/';
   }
 
   vergroessereFeld() {

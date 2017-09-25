dd6fd682b57418d3b3e05f5d7dd871fde72f575d
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2017-09-25 11:20:17 +0200

    weitere aenderungen

---
+++ src/client/app/suche/basic-search/basic-search.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  moduleId: module.id,
+  selector: 'rae-basic-search',
+  templateUrl: './basic-search.component.html',
+  styleUrls: ['./basic-search.component.css']
+})
+export class BasicSearchComponent {
+
+  hideSearchfield: boolean = true;
+  placeholder = 'Suche...';
+
+  constructor(private router: Router) {
+    router.events.subscribe(changes => {
+      this.hideSearchfield = true;
+      this.placeholder = 'Suche...';
+    });
+  }
+
+  sendRequest(values: any) {
+    this.router.navigateByUrl('/suche/' + encodeURIComponent(values),);
+  }
+
+
+}

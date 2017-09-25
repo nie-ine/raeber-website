fdf794567695a58a1526067000712b4598c25e56
rfbaumgartner <rfbaumgartner@users.noreply.github.com>
2017-09-25 09:33:08 +0200

    delete steckbrief fields if no value can be returned

--- src/client/app/konvolut/konvolut-steckbrief/konvolut-steckbrief.component.ts
+++ src/client/app/konvolut/konvolut-steckbrief/konvolut-steckbrief.component.ts
@@ -46,56 +46,67 @@ export class KonvolutSteckbriefComponent implements OnChanges {
           try {
             this.publicationTitle = res.props[ 'http://www.knora.org/ontology/work#hasPublicationDescription' ].values[ 0 ].utf8str;
           } catch (TypeError) {
+            this.publicationTitle = null;
           }
 
           try {
             this.publisherDescription = res.props[ 'http://www.knora.org/ontology/work#hasPublisherDescription' ].values[ 0 ].utf8str;
           } catch (TypeError) {
+            this.publisherDescription = null;
           }
 
           try {
             this.printerDescription = res.props[ 'http://www.knora.org/ontology/work#hasPrinterDescription' ].values[ 0 ].utf8str;
           } catch (TypeError) {
+            this.printerDescription = null;
           }
 
           try {
             this.convoluteDescription = res.props[ 'http://www.knora.org/ontology/text#hasConvoluteDescription' ].values[ 0 ].utf8str;
           } catch (TypeError) {
+            this.convoluteDescription = null;
           }
 
           try {
             this.carrierDescription = res.props[ 'http://www.knora.org/ontology/text#hasCarrierDescription' ].values[ 0 ].utf8str;
           } catch (TypeError) {
+            this.carrierDescription = null;
           }
 
           try {
             this.comment = res.props[ 'http://www.knora.org/ontology/text#hasComment' ].values[ 0 ].utf8str;
           } catch (TypeError) {
+            this.comment = null;
           }
 
           try {
             this.archiveSignature = res.props[ 'http://www.knora.org/ontology/work#hasArchiveSignature' ].values[ 0 ].utf8str;
           } catch (TypeError) {
+            this.archiveSignature = null;
           }
 
           try {
-            this.archiveSignature = res.props[ 'http://www.knora.org/ontology/text#hasConvoluteSizeDescription' ].values[ 0 ].utf8str;
+            this.convoluteSizeDescripton = res.props[ 'http://www.knora.org/ontology/text#hasConvoluteSizeDescription' ].values[ 0 ].utf8str;
           } catch (TypeError) {
+            this.convoluteSizeDescripton = null;
           }
 
           try {
             this.convoluteContentRepresentation = res.props[ 'http://www.knora.org/ontology/text#hasConvoluteContentRepresentation' ].values[ 0 ].utf8str;
           } catch (TypeError) {
+            this.convoluteContentRepresentation = null;
           }
 
           try {
             this.convoluteOriginDescription = res.props[ 'http://www.knora.org/ontology/text#hasConvoluteOriginDescription' ].values[ 0 ].utf8str;
           } catch (TypeError) {
+            this.convoluteOriginDescription = null;
           }
 
           try {
             this.creatingPeriod = res.props[ 'http://www.knora.org/ontology/human#hasCreatingPeriod' ].values[ 0 ];
           } catch (TypeError) {
+            this.creatingPeriod = null;
           }
 
         });
@@ -104,16 +115,18 @@ export class KonvolutSteckbriefComponent implements OnChanges {
     if (true) {
       this.sub2 = this.http.get('http://knora.nie-ine.ch/v1/resources/' + encodeURIComponent('http://rdfh.ch/human/YNOD3dS_RJas5Oj0XjA51w'
     ))
-        .map(response => response.json()).subscribe(res =>{
+        .map(response => response.json()).subscribe(res => {
 
           try {
             this.creatingPeriodStart = res.props[ 'http://www.knora.org/ontology/event#hasStartDate' ].values[ 0 ].dateval1;
           } catch (TypeError) {
+            this.creatingPeriodStart = null;
           }
 
           try {
             this.creatingPeriodEnd = res.props[ 'http://www.knora.org/ontology/event#hasEndDate' ].values[ 0 ].dateval1;
           } catch (TypeError) {
+            this.creatingPeriodEnd = null;
           }
         });
 

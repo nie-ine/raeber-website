4dfbee0c2fa7f258eb811d49a8041a25385f4bb9
rfbaumgartner <rfbaumgartner@users.noreply.github.com>
2017-09-28 12:02:03 +0200

    correct request so that 'Publikationen' now works

--- src/client/app/konvolut/konvolut-steckbrief/konvolut-steckbrief-publikation.component.ts
+++ src/client/app/konvolut/konvolut-steckbrief/konvolut-steckbrief-publikation.component.ts
@@ -10,7 +10,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 @Component({
   moduleId: module.id,
   selector: 'rae-konvolut-steckbrief-publikation',
-  template: '<span><a [routerLink]="[konvolutTitel]">{{ konvolutTitel }}</a> ({{ konvolutSize}} Gedichte)</span>'
+  template: '<span><a [routerLink]="[\'/\' + konvolutTitel]">{{ konvolutTitel }}</a> ({{ konvolutSize}} Gedichte)</span>{{res}}'
 })
 export class KonvolutSteckbriefPublikationComponent implements OnChanges {
 
@@ -28,14 +28,14 @@ export class KonvolutSteckbriefPublikationComponent implements OnChanges {
         .map(response => response.json()).subscribe(res => {
 
           try {
-            this.konvolutTitel = res.props[ 'http://www.knora.org/ontology/work#hasPublicationDescription' ].values[ 0 ].utf8str;
+            this.konvolutTitel = res.props[ 'http://www.knora.org/ontology/text#hasConvoluteTitle' ].values[ 0 ].utf8str;
           } catch (TypeError) {
             this.konvolutTitel = null;
           }
 
           try {
             let searchParams = new ExtendedSearch();
-            searchParams.property = new KnoraProperty('http://www.knora.org/ontology/work#isPublishedIn', '!EQ', this.konvolutIRI);
+            searchParams.property = new KnoraProperty('http://www.knora.org/ontology/work#isPublishedIn', 'EQ', this.konvolutIRI);
             searchParams.showNRows = 500;
 
             this.route.params

ff0b139651307b451700b209d6b7640c68025c80
Sebastian <sebastian.schuepbach@unibas.ch>
2017-10-06 10:51:08 +0200

    First step for a generic "Synopse" request in synopse.component.ts

--- src/client/app/synopse/synopse.component.ts
+++ src/client/app/synopse/synopse.component.ts
@@ -4,9 +4,7 @@
 
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
-import { ActivatedRoute, Params } from '@angular/router';
-
-import { ExtendedSearch, KnoraProperty } from '../shared/utilities/knora-api-params';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   moduleId: module.id,
@@ -15,7 +13,7 @@ import { ExtendedSearch, KnoraProperty } from '../shared/utilities/knora-api-par
 })
 export class SynopseComponent implements OnInit {
 
-  poemsInSynopse: Array<any>;
+  poemsInSynopse: Array<any> = [];
   nHits: number;
   synopseTag: string;
 
@@ -23,11 +21,15 @@ export class SynopseComponent implements OnInit {
 
   columns: string;
   gridHeight: number = 0;
+  workIri: string;
+
+  results: number;
 
   private sub: any;
 
-  constructor(private http: Http, private route: ActivatedRoute) {
+  constructor(private http: Http, private route: ActivatedRoute, router: Router) {
     this.showText = true;
+    this.workIri = router.url.split('/')[ 2 ];
   }
 
   ngOnInit() {
@@ -35,22 +37,28 @@ export class SynopseComponent implements OnInit {
       this.synopseTag = params[ 'synopse' ];
     });
 
-    // TODO Parameter anpassen
-    let searchParams = new ExtendedSearch();
-    searchParams.filterByRestype = 'http://www.knora.org/ontology/text#Convolute';
-    // searchParams.property = new KnoraProperty('http://www.knora.org/ontology/text#hasTitle', 'MATCH', this.synopseTag);
-    searchParams.property = new KnoraProperty('http://www.knora.org/ontology/text#hasTitle', '!EQ', ' ');
-    searchParams.property = new KnoraProperty('http://www.knora.org/ontology/text#hasDescription', '!EQ', ' ');
-    searchParams.showNRows = 500;
+    let searchParamsPrefix = 'http://knora.nie-ine.ch/v1/resources/';
+    let searchParamsWork = searchParamsPrefix + encodeURIComponent('http://rdfh.ch/kuno-raeber/' + this.workIri);
+    let poemsIri: string[] = [];
 
     this.route.params
       .switchMap((params: Params) =>
-        this.http.get(searchParams.toString()))
+        this.http.get(searchParamsWork))
       .map(response => response.json())
       .subscribe((res: any) => {
-        this.poemsInSynopse = res.subjects;
-        this.nHits = res.nhits;
-        console.log(res.nhits);
+        poemsIri = res.props[ 'http://www.knora.org/ontology/work#isExpressedIn' ].values;
+        this.results = poemsIri.length;
+        poemsIri.forEach(poemIri => {
+          console.log(searchParamsPrefix + encodeURIComponent(poemIri));
+          this.route.params
+            .switchMap((params: Params) =>
+              this.http.get(searchParamsPrefix + encodeURIComponent(poemIri)))
+            .map(response => response.json())
+            .subscribe((res: any) => {
+              // FIXME: res should probably be transformed in a form more suitable for textgrid
+              this.poemsInSynopse.push(res);
+            });
+        });
       });
   }
 

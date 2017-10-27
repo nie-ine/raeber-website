aeb850fb439db25ca15a26afdefa14c7828218d1
padlina <roberta.padlina@unibas.ch>
2017-10-27 17:11:21 +0200

    Added missing items and content for menu "Infos".

---
+++ src/client/app/statisch/werkliste-unselbst.component.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by Roberta Padlina (roberta.padlina@unibas.ch) on 27/10/17.
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'rae-werkliste-unselbst',
+  templateUrl: 'werkliste-unselbst.component.html'
+})
+export class WerklisteUnselbstComponent {
+  title = 'Unselbständige Publikationen';
+}

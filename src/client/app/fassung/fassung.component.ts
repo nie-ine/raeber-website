460df3136c66651b205301967a25b8928c652670
Sebastian Schüpbach <sebastian.schuepbach@unibas.ch>
2017-10-29 00:41:00 +0200

    Update breadcrumbs in Fassungsansicht when navigating through poems

--- src/client/app/fassung/fassung.component.ts
+++ src/client/app/fassung/fassung.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewChecked, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
@@ -69,12 +69,6 @@ export class FassungComponent implements OnInit, AfterViewChecked {
     return searchParams.toString();
   }
 
-  constructor(private http: Http,
-              private router: Router,
-              private cdr: ChangeDetectorRef,
-              private dfs: DateFormatService) {
-  }
-
   private static buildRouteTitleStringFromResultSet(resultSet: any, convoluteTitle: string) {
     const poemShortIRI = resultSet.subjects[ 0 ].value[ 3 ].split('/')[ 4 ];
     const poemTitle = resultSet.subjects[ 0 ].value[ 4 ];
@@ -88,11 +82,19 @@ export class FassungComponent implements OnInit, AfterViewChecked {
       element;
   }
 
+  constructor(private http: Http,
+              private route: ActivatedRoute,
+              private cdr: ChangeDetectorRef,
+              private dfs: DateFormatService) {
+    route.params.subscribe(p => {
+      this.convoluteTitle = p.konvolut;
+      this.poemShortIri = p.fassung.split('---')[ 1 ];
+    });
+  }
+
   ngOnInit() {
     this.poem_resizable = true;
     this.show_register = true;
-    this.convoluteTitle = decodeURIComponent(this.router.url.split('/')[ 1 ]);
-    this.poemShortIri = this.router.url.split('/')[ 2 ].split('---')[ 1 ];
     this.getDataFromDB();
     this.buildLinkToRelatedConvolute();
   }

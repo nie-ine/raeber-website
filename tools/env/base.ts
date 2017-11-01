a32abcff021af730ed3ee105920adafd8a99b2ee
padlina <roberta.padlina@unibas.ch>
2017-11-01 12:15:30 +0100

    First release 1.0.0 ! Yeah !

--- tools/env/base.ts
+++ tools/env/base.ts
@@ -3,8 +3,8 @@ import { EnvConfig } from './env-config.interface';
 const BaseConfig: EnvConfig = {
   // Sample API url
   API: 'https://demo.com',
-  RELEASEURL: 'https://github.com/nie-ine/raeber-website/releases/tag/0.18.1',
-  RELEASEVERSION: '0.18.1'
+  RELEASEURL: 'https://github.com/nie-ine/raeber-website/releases/tag/1.0.0',
+  RELEASEVERSION: '1.0.0'
 };
 
 export = BaseConfig;

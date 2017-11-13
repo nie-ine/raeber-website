7510419a199f09b552f743633e9b997a351c12fb
Sebastian Schüpbach <sebastian.schuepbach@unibas.ch>
2017-11-13 14:20:19 +0100

    Replace static afterword of "Abgewandt Zugewandt" with a dynamic view

--- src/client/app/core/navigationsleiste.component.ts
+++ src/client/app/core/navigationsleiste.component.ts
@@ -7,6 +7,7 @@ import { Http, Response } from '@angular/http';
 import { AbstractControl } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { globalSearchVariableService } from '../suche/globalSearchVariablesService';
 
 @Component({
   moduleId: module.id,
@@ -14,20 +15,46 @@ import { Location } from '@angular/common';
   templateUrl: './navigationsleiste.component.html'
 })
 export class NavigationsleisteComponent implements OnInit {
-  constructor( private http: Http, private route: ActivatedRoute, private location: Location) {
+  abgewandtZugewandtNachwortIri: string;
+
+  constructor(private http: Http, private route: ActivatedRoute, private location: Location) {
     this.route.params.subscribe(params => console.log(params));
   }
 
   ngOnInit() {
     this.createLinkToSearch();
+    this.getLinkToAbgewandtZugewandtNachwort();
     console.log(this.route.snapshot.queryParams[ 'wort' ]);
   }
+
   createLinkToSearch() {
-      //console.log(this.route._routerState.snapshot.url);
-      // if(this.route._routerState.snapshot.url === '/suche') {
-      if(this.route) {
-        return 'resetSuche';
-      } else return 'suche';
+    //console.log(this.route._routerState.snapshot.url);
+    // if(this.route._routerState.snapshot.url === '/suche') {
+    if (this.route) {
+      return 'resetSuche';
+    } else return 'suche';
+  }
+
+  private getLinkToAbgewandtZugewandtNachwort() {
+    const request = globalSearchVariableService.API_URL +
+      globalSearchVariableService.extendedSearch +
+      'http%3A%2F%2Fwww.knora.org%2Fontology%2Fkuno-raeber-gui%23Poem' +
+      '&property_id=http%3A%2F%2Fwww.knora.org%2Fontology%2Fkuno-raeber-gui%23hasConvoluteTitle' +
+      '&compop=EQ' +
+      '&searchval=Abgewandt Zugewandt (Nachwort)' +
+      '&property_id=http%3A%2F%2Fwww.knora.org%2Fontology%2Fkuno-raeber-gui%23hasPoemTitle' +
+      '&compop=EQ' +
+      '&searchval=Nachwort über das schweizerische Sprachdilemma' +
+      '&property_id=http%3A%2F%2Fwww.knora.org%2Fontology%2Fkuno-raeber-gui%23hasPoemIRI' +
+      '&compop=EXISTS' +
+      '&searchval=' +
+      '&show_nrows=1';
+    console.log(request);
+    return this.http.get(request)
+      .map(lambda => lambda.json())
+      .subscribe(res => this.abgewandtZugewandtNachwortIri =
+        '/Abgewandt Zugewandt (Nachwort)/Nachwort über das schweizerische Sprachdilemma---' +
+        res.subjects[ 0 ].value[ 2 ].split('/')[ 4 ]);
   }
 
 }

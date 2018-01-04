9b7ee514167575097046af369ae1c6389c9e9811
Sebastian Schüpbach <sebastian.schuepbach@unibas.ch>
2018-01-04 16:33:05 +0100

    Increase release version number to 1.0.4

--- tools/env/base.ts
+++ tools/env/base.ts
@@ -3,8 +3,8 @@ import { EnvConfig } from './env-config.interface';
 const BaseConfig: EnvConfig = {
   // Sample API url
   API: 'https://demo.com',
-  RELEASEURL: 'https://github.com/nie-ine/raeber-website/releases/tag/1.0.3',
-  RELEASEVERSION: '1.0.3'
+  RELEASEURL: 'https://github.com/nie-ine/raeber-website/releases/tag/1.0.4',
+  RELEASEVERSION: '1.0.4'
 };
 
 export = BaseConfig;

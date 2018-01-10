1597add8f27b7a753a10495cc73ced97f83924cf
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2018-01-10 14:19:37 +0100

    outcomment console.log

--- src/client/app/suche/suche.component.ts
+++ src/client/app/suche/suche.component.ts
@@ -890,7 +890,7 @@ export class SucheComponent implements OnInit, AfterViewChecked {
   }
 
   updateSuchmaskeKonvolutIRIMapping(arg: AbstractControl) {
-    console.log(arg);
+    //console.log(arg);
     if (
       arg.get('notizbuchForm').pristine
       && arg.get('druckForm').pristine

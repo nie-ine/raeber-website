7e0e99d25890d9d28c5335d21b6652872aa623bb
Sebastian Schüpbach <sebastian.schuepbach@unibas.ch>
2018-01-15 14:56:26 +0100

    NIE-260: Re-implement help dialog on homepage as popup

--- src/client/app/statisch/anleitung.component.ts
+++ src/client/app/statisch/anleitung.component.ts
@@ -2,7 +2,8 @@
  * Created by Sebastian Schüpbach (sebastian.schuepbach@unibas.ch) on 6/7/17.
  */
 
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MD_DIALOG_DATA, MdDialogRef } from '@angular/material';
 
 @Component({
   moduleId: module.id,
@@ -11,4 +12,12 @@ import { Component } from '@angular/core';
 })
 export class AnleitungComponent {
   title = 'Anleitung zur Online-Edition Kuno Raeber';
+
+  constructor(public dialogRef: MdDialogRef<AnleitungComponent>, @Inject(MD_DIALOG_DATA) public data: any) {
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
 }

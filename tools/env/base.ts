304c3d7801dd788cb54ca32968ee5aea49538f50
padlina <roberta.padlina@unibas.ch>
2018-04-26 14:47:11 +0200

    Update release version to 1.0.6

--- tools/env/base.ts
+++ tools/env/base.ts
@@ -3,8 +3,8 @@ import { EnvConfig } from './env-config.interface';
 const BaseConfig: EnvConfig = {
   // Sample API url
   API: 'https://demo.com',
-  RELEASEURL: 'https://github.com/nie-ine/raeber-website/releases/tag/1.0.5',
-  RELEASEVERSION: '1.0.5'
+  RELEASEURL: 'https://github.com/nie-ine/raeber-website/releases/tag/1.0.6',
+  RELEASEVERSION: '1.0.6'
 };
 
 export = BaseConfig;

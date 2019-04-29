bffd966e02436b94d747361559df3201bba3df86
domsteinbach <dominique.steinbach@unibas.ch>
2019-04-29 14:12:04 +0200

    debugging: changed properties acc. to Ontology classes/properties

--- src/client/app/shared/utilities/iris.ts
+++ src/client/app/shared/utilities/iris.ts
@@ -1,12 +1,12 @@
 export enum Event {
-  hasStartDate = 'http://www.knora.org/ontology/0047/event#hasStartDate',
-  hasEndDate = 'http://www.knora.org/ontology/0047/event#hasEndDate'
+  hasStartDate = 'http://www.knora.org/ontology/shared/event#hasStartDate',
+  hasEndDate = 'http://www.knora.org/ontology/shared/event#hasEndDate'
 }
 
 export enum Human {
-  hasCreatingPeriod = 'http://www.knora.org/ontology/0048/human#hasCreating',   
-  hasCreationDate = 'http://www.knora.org/ontology/0048/human#creationHasDate',
-  hasModificationDate = 'http://www.knora.org/ontology/0048/human#hasModificationDate'
+  hasCreatingPeriod = 'http://www.knora.org/ontology/shared/human#hasCreating',
+  hasCreationDate = 'http://www.knora.org/ontology/shared/human#creationHasDate',
+  hasModificationDate = 'http://www.knora.org/ontology/shared/human#hasModificationDate'
 }
 
 export enum KunoRaeber {
@@ -15,22 +15,22 @@ export enum KunoRaeber {
   containsLaterStagesOfManuscriptConvolute = 'http://www.knora.org/ontology/004D/kuno-raeber#containsLaterStagesOfManuscriptConvolute',
   containsLaterStagesOfNotebook = 'http://www.knora.org/ontology/004D/kuno-raeber#containsLaterStagesOfNotebook',
   containsLaterStagesOfTypescriptConvolute = 'http://www.knora.org/ontology/004D/kuno-raeber#containsLaterStagesOfTypescriptConvolute',
-  hasDiplomaticTranscription = 'http://www.knora.org/ontology/005A/text-editing#hasDiplomaticTranscription',
-  hasEdition = 'http://www.knora.org/ontology/005A/text-editing#hasEdition',
+  hasDiplomaticTranscription = 'http://www.knora.org/ontology/shared/text-editing#hasDiplomaticTranscription',
+  hasEdition = 'http://www.knora.org/ontology/shared/text-editing#hasEdition',
   hasReferencePoem = 'http://www.knora.org/ontology/004D/kuno-raeber#hasReferencePoem',
   hasReferenceTitle = 'http://www.knora.org/ontology/004D/kuno-raeber#hasReferenceTitle',
-  hasSameEditionAs = 'http://www.knora.org/ontology/005A/text-editing#hasSameEditionAs',
+  hasSameEditionAs = 'http://www.knora.org/ontology/shared/text-editing#hasSameEditionAs',
   isInDiary = 'http://www.knora.org/ontology/004D/kuno-raeber#isInDiary',
-  isInManuscript = 'http://www.knora.org/004C/information-carrier#isInManuscript',
+  isInManuscript = 'http://www.knora.org/shared/information-carrier#isInManuscript',
   isInNotebook = 'http://www.knora.org/ontology/004D/kuno-raeber#isInNotebook',
-  isInTypescript = 'http://www.knora.org/ontology/004C/information-carrier#isInTypescript',
+  isInTypescript = 'http://www.knora.org/ontology/shared/information-carrier#isInTypescript',
   DiaryConvolute = 'http://www.knora.org/ontology/004D/kuno-raeber#DiaryConvolute',
   HandwrittenPoem = 'http://www.knora.org/ontology/004D/kuno-raeber#HandwrittenPoem',
-  PoemManuscriptConvolute = 'http://www.knora.org/ontology/004D/kuno-raeber#HandwrittenPoem',
+  PoemManuscriptConvolute = 'http://www.knora.org/ontology/004D/kuno-raeber#ManuscriptConvolute',
   PoemNote = 'http://www.knora.org/ontology/004D/kuno-raeber#PoemNote',
   PoemNotebook = 'http://www.knora.org/ontology/004D/kuno-raeber#PoemNotebook',
   PoemPostcardConvolute = 'http://www.knora.org/ontology/004D/kuno-raeber#PoemPostcardConvolute',
-  PoemTypescriptConvolute = 'http://www.knora.org/ontology/004D/kuno-raeber#PoemTypescriptConvolute',
+  PoemTypescriptConvolute = 'http://www.knora.org/ontology/004D/kuno-raeber#TypescriptConvolute',
   PolyAuthorPublicationConvolute = 'http://www.knora.org/ontology/004D/kuno-raeber#PolyAuthorPublicationConvolute',
   PrintedPoemBookPublication = 'http://www.knora.org/ontology/004D/kuno-raeber#PrintedPoemBookPublication',
   PublicationPoem = 'http://www.knora.org/ontology/004D/kuno-raeber#PublicationPoem',
@@ -41,14 +41,14 @@ export enum KunoRaeberGUI {
   hasAlphabeticIndex = 'http://www.knora.org/ontology/004E/kuno-raeber-gui#hasAlphabeticIndex',
   hasConvoluteIri = 'http://www.knora.org/ontology/004E/kuno-raeber-gui#hasConvoluteIRI',
   hasConvoluteTitle = 'http://www.knora.org/ontology/004E/kuno-raeber-gui#hasConvoluteTitle',
-  hasDateIndex = 'http://www.knora.org/ontology/004E/kuno-raeber-gui#hasDateIndex',
+  hasDateIndex = 'http://www.knora.org/ontology/004E/kuno-raeber-gui#hasPublishingStateIndex', // WHAT'S THAT THING ?hasDateIndex --> hasPublishingStateIndex
   hasPoemCreationDate = 'http://www.knora.org/ontology/004E/kuno-raeber-gui#hasPoemCreationDate',
   hasPoemIri = 'http://www.knora.org/ontology/004E/kuno-raeber-gui#hasPoemIRI',
   hasPoemText = 'http://www.knora.org/ontology/004E/kuno-raeber-gui#hasPoemText',
   hasPoemTitle = 'http://www.knora.org/ontology/004E/kuno-raeber-gui#hasPoemTitle',
   hasSameEditionAs = 'http://www.knora.org/ontology/004E/kuno-raeber-gui#hasSameEditionAs',
   hasStrophe = 'http://www.knora.org/ontology/004E/kuno-raeber-gui#hasStrophe',
-  hasStructure = 'http://www.knora.org/ontology/004E/kuno-raeber-gui#hasStructure',
+  hasStructure = 'http://www.knora.org/ontology/004E/kuno-raeber-gui#hasRhyme', // Changed
   hasSynopsisIri = 'http://www.knora.org/ontology/004E/kuno-raeber-gui#hasSynopsisIRI',
   hasSynopsisTitle = 'http://www.knora.org/ontology/004E/kuno-raeber-gui#hasSynopsisTitle',
   isFinalVersion = 'http://www.knora.org/ontology/004E/kuno-raeber-gui#isFinalVersion',
@@ -63,43 +63,43 @@ export enum KnoraBase {
 }
 
 export enum Text {
-  containsEarlierStagesOfPublication = 'http://www.knora.org/ontology/004C/information-carrier#containsEarlierStagesOfPublication',
-  hasAlias = 'http://www.knora.org/ontology/0059/text#hasAlias',
-  hasCarrierCollectionDescription = 'http://www.knora.org/ontology/004C/information-carrier#hasCarrierDescription', 
-  hasCarrierDescription = 'http://www.knora.org/ontology/004C/information-carrier#carrierHasDescription', //WHAT about this?
-  hasComment = 'http://www.knora.org/ontology/0059/text#hasComment',
-  hasContent = 'http://www.knora.org/ontology/004F/language#hasContent',
-  hasConvoluteContentRepresentation = 'http://www.knora.org/ontology/004C/information-carrier#convoluteHasContentRepresentation',
-  hasConvoluteDescription = 'http://www.knora.org/ontology/004C/information-carrier#ConvolutehasDescription',
-  hasConvoluteOriginDescription = 'http://www.knora.org/ontology/004C/information-carrier#convoluteHasOriginDescription',
-  hasConvoluteSizeDescription = 'http://www.knora.org/ontology/004C/information-carrier#convoluteHasSizeDescription',
-  hasConvoluteTitle = 'http://www.knora.org/ontology/004C/information-carrier#convoluteHasTitle',
-  hasDetailDescription = 'http://www.knora.org/ontology/0059/text#hasDetailDescription',
-  hasEnteringDate = 'http://www.knora.org/ontology/004C/information-carrier#hasDiaryEnteringDate',
-  hasPublicationNumber = 'http://www.knora.org/ontology/0056/publishing#hasPublicationNumber',
-  hasSpecialDescription = 'http://www.knora.org/ontology/0059/text#hasSpecialDescription',
+  containsEarlierStagesOfPublication = 'http://www.knora.org/ontology/shared/information-carrier#containsEarlierStagesOfPublication',
+  hasAlias = 'http://www.knora.org/ontology/shared/text#hasAlias',
+  hasCarrierCollectionDescription = 'http://www.knora.org/ontology/shared/information-carrier#hasCarrierDescription',
+  hasCarrierDescription = 'http://www.knora.org/ontology/shared/information-carrier#carrierHasDescription', //WHAT about this? It's here but changed directions?
+  hasComment = 'http://www.knora.org/ontology/shared/text#hasComment',
+  hasContent = 'http://www.knora.org/ontology/shared/language#hasContent',
+  hasConvoluteContentRepresentation = 'http://www.knora.org/ontology/shared/information-carrier#convoluteHasContentRepresentation',
+  hasConvoluteDescription = 'http://www.knora.org/ontology/shared/information-carrier#ConvolutehasDescription',
+  hasConvoluteOriginDescription = 'http://www.knora.org/ontology/shared/information-carrier#convoluteHasOriginDescription',
+  hasConvoluteSizeDescription = 'http://www.knora.org/ontology/shared/information-carrier#convoluteHasSizeDescription',
+  hasConvoluteTitle = 'http://www.knora.org/ontology/shared/information-carrier#convoluteHasTitle',
+  hasDetailDescription = 'http://www.knora.org/ontology/shared/text#hasDetailDescription',
+  hasEnteringDate = 'http://www.knora.org/ontology/shared/information-carrier#hasDiaryEnteringDate',
+  hasPublicationNumber = 'http://www.knora.org/ontology/shared/publishing#hasPublicationNumber',
+  hasSpecialDescription = 'http://www.knora.org/ontology/shared/text#hasSpecialDescription',
   hasStrophe = 'http://www.knora.org/ontology/004D/kuno-raeber#hasStrophe',
-  hasStructure = 'http://www.knora.org/ontology/0059/text#hasStructure', // HERE a whole new ontology has been defined and no equivalent existing ... !!!
-  hasTitle = 'http://www.knora.org/ontology/0059/text#hasTitle',
-  isDiplomaticTranscriptionOfTextOnPage = 'http://www.knora.org/ontology/005A/text-editing#isDiplomaticTranscriptionOfTextOnPage',
+  hasStructure = 'http://www.knora.org/ontology/shared/text#hasStructure', // HERE a whole new ontology has been defined and no equivalent existing ... !!!
+  hasTitle = 'http://www.knora.org/ontology/shared/text#hasTitle',
+  isDiplomaticTranscriptionOfTextOnPage = 'http://www.knora.org/ontology/shared/text-editing#isDiplomaticTranscriptionOfTextOnPage',
   isFinalVersion = 'http://www.knora.org/ontology/004D/kuno-raeber#isFinalVersion',
-  isInDialect = 'http://www.knora.org/ontology/004E/kuno-raeber-gui#isInDialect',
+  isInDialect = 'http://www.knora.org/ontology/004E/kuno-raeber-gui#isInSwissGerman', // CHANGED .,..
   isPartOfCycle = 'http://www.knora.org/ontology/004D/kuno-raeber#isPartOfCycle',
-  isWrittenWith = 'http://www.knora.org/ontology/004C/information-carrier#isWrittenWith'
+  isWrittenWith = 'http://www.knora.org/ontology/shared/information-carrier#isWrittenWith'
 }
 
 export enum Work {
-  hasPublisherDescription = 'http://www.knora.org/ontology/0056/publishing#hasPublisherDescription',
-  hasPrinterDescription = 'http://www.knora.org/ontology/0056/publishing#hasPrinterDescription',
-  hasArchiveSignature = 'http://www.knora.org/ontology/004C/information-carrier#hasArchiveSignature',
-  hasLastAuthorizedPublication = 'http://www.knora.org/ontology/0056/publishing#hasLastAuthorizedPublication',
-  hasNachlassPublicationDescription = 'http://www.knora.org/ontology/0056/publishing#hasNachlassPublicationDescription',
+  hasPublisherDescription = 'http://www.knora.org/ontology/shared/publishing#hasPublisherDescription',
+  hasPrinterDescription = 'http://www.knora.org/ontology/shared/publishing#hasPrinterDescription',
+  hasArchiveSignature = 'http://www.knora.org/ontology/shared/information-carrier#hasArchiveSignature',
+  hasLastAuthorizedPublication = 'http://www.knora.org/ontology/shared/publishing#hasLastAuthorizedPublication',
+  hasNachlassPublicationDescription = 'http://www.knora.org/ontology/shared/publishing#hasNachlassPublicationDescription',
   hasPageNumber = 'http://www.knora.org/ontology/004E/kuno-raeber-gui#hasPageNumber',
-  hasPageNumberDescription = 'http://www.knora.org/ontology/0059/text#hasPageNumberDescription',
-  hasPublicationDescription = 'http://www.knora.org/ontology/0056/publishing#hasDescription',
-  hasPublicationTitle = 'http://www.knora.org/ontology/0056/publishing#hasTitle',
-  hasPublishingState = 'http://www.knora.org/ontology/0056/publishing#hasPublishingState',
-  hasUnauthorizedPublication = 'http://www.knora.org/ontology/0056/publishing#hasUnauthorizedPublication',
-  isExpressedIn = 'http://www.knora.org/ontology/0044/concept#isExpressedIn',
-  isPublishedIn = 'http://www.knora.org/ontology/0056/publishing#isPublishedIn'
+  hasPageNumberDescription = 'http://www.knora.org/ontology/shared/text#hasPageNumberDescription',
+  hasPublicationDescription = 'http://www.knora.org/ontology/shared/publishing#hasDescription',
+  hasPublicationTitle = 'http://www.knora.org/ontology/shared/publishing#hasTitle',
+  hasPublishingState = 'http://www.knora.org/ontology/shared/publishing#hasPublishingState',
+  hasUnauthorizedPublication = 'http://www.knora.org/ontology/shared/publishing#hasUnauthorizedPublication',
+  isExpressedIn = 'http://www.knora.org/ontology/shared/concept#isExpressedIn',
+  isPublishedIn = 'http://www.knora.org/ontology/shared/publishing#isPublishedIn'
 }
